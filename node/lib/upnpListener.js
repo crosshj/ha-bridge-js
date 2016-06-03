580d3642de448e06f51b9674de5a0c851c06664c
HJ Cross <github@chimpjuice.com>
2016-06-02 22:35:00 -0400

    refactor / rename upnp stuff, add console.log

--- node/lib/upnpListener.js
+++ node/lib/upnpListener.js
@@ -13,7 +13,7 @@ var options = {
 var server = new Server(options);
 
 // the following is an overrride because node-ssdp is a little too rigid here
-server._respondToSearch = require('./customResponse');
+server._respondToSearch = require('./upnpCustomResponse');
 server._nls = uuid;
 server.addUSN('urn:schemas-upnp-org:device:basic:1');
 server.addUSN('uuid:Socket-1_0-221438K0100073::urn:Belkin:device:**\r\n\r\n');
