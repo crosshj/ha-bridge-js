20f0afe5b363cf351e872dcf049a445531d2a772
crosshj <github@chimpjuice.com>
2017-02-08 02:56:39 -0500

    build UI for hubs, refactor and reformat

--- client/components/app.js
+++ client/components/app.js
@@ -4,13 +4,57 @@ import Body from './body';
 import Duplicate from './duplicate';
 
 class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      hubs: ['Milight', 'Wink', 'Hue', 'Generic', 'All']
+    };
+    this.handleHubChange = this.handleHubChange.bind(this);
+    this.handleAddHub = this.handleAddHub.bind(this);
+  }
+
+  handleHubChange(selectedHub){
+    this.setState({selectedHub});
+  }
+
+  handleAddHub(newHub){
+    if(!newHub){
+      this.setState({newHub: {
+        name: '',
+        url: '',
+        types: ['foo', 'bar', 'baz']
+      }});
+    } else {
+      this.setState({
+        newHub: undefined,
+        hubs: (newHub === 'cancel')
+          ? this.state.hubs
+          : [newHub.name].concat(this.state.hubs)
+      });
+    }
+  }
+
   render () {
+    const props = {
+      hubs: this.state.hubs,
+      selected: {
+        hub: this.state.selectedHub || 'All'
+      },
+      devices: [
+        {name: '_den', hub: 'Milight'},
+        {name: '_master_b_room', hub: 'Milight'},
+        {name: 'WifiPlugOne', hub: 'Generic'}
+      ],
+      newHub: this.state.newHub,
+      handleAddHub: this.handleAddHub,
+      handleHubChange: this.handleHubChange
+    };
     return <div>
         {
         // <Menu />
         // <Body />
-    	}
-        <Duplicate />
+      }
+        <Duplicate {...props}/>
     </div>
 
   }
--- client/components/duplicate.js
+++ client/components/duplicate.js
@@ -1,134 +1,211 @@
 import React from 'react';
 
-function duplicate(props){
-	const duplicate = (
-		<div>
-			<nav className="navbar navbar-inverse navbar-fixed-top">
-			    <div className="container">
-			        <div className="navbar-header">
-			            <a className="navbar-brand" href="#">HA Bridge Configuration</a>
-			        </div>
-			    </div>
-			</nav>
-			<div className="container">
-				<div>
-			        <div className="panel panel-default bridgeServer">
-			            <div className="panel-heading">
-			            	<h1 className="panel-title">Bridge settings</h1>
-		            	</div>
-			            <div className="panel-body">
-			                <form className="form">
-			                    <div className="col-xs-7 col-sm-7">
-			                        <input id="bridge-base" className="form-control" type="text" placeholder="URL to bridge" />
-			                    </div>
-			                    <div className="btn-toolbar">
-			                        <button type="submit" className="col-xs-2 col-sm-1 btn btn-primary btn">
-			                        	Load
-			                        </button>
-			                        <button type="submit" className="col-xs-2 col-sm-1 btn btn-primary">
-			                        	Go
-			                        </button>
-			                    </div>
-			                </form>
-			            </div>
-			        </div>
+var tempHub = {
+  name: 'TODO: change tempHub'
+};
 
-              <div>
-                  <div className="panel panel-default">
-                      <div className="panel-heading">
-                        <h2 className="panel-title">Hubs</h2>
-                      </div>
-                      <div className="panel-body">
-                        <div className="form-group">
-                            <div className="col-xs-1"></div>
-                            <div className="col-xs-11">
-                              { ['Milight', 'Wink', 'Hue', 'Default'].map((name, key) => {
-                                return (
-                                  <label className="radio" key={key}>
-                                      <input value={key} type="radio" name="radio" defaultChecked={name==='Default'}/>{name}
-                                  </label>
-                                );
-                              })}
-                            </div>
-                        </div>
-                      </div>
-                  </div>
+function duplicate({
+  selected={}, hubs=[], devices=[], newHub,
+  handleHubChange = () => {},
+  handleAddHub = () => {}
+}){
+  const hubChange = (event) => {
+    const hub = event.target.value;
+    handleHubChange(hub);
+  };
+
+  const handleAddHubClick = (event) => {
+    var newHub = undefined;
+    if(!!~event.target.className.indexOf('submit-hub')){
+      newHub = JSON.parse(JSON.stringify(tempHub));
+    }
+    if(!!~event.target.className.indexOf('cancel-hub')){
+      newHub = 'cancel';
+    }
+    handleAddHub(newHub);
+  };
+
+  //TODO: change tempHub when newHub dialog changes
+
+  const currentDevices = selected.hub === 'All'
+    ? devices
+    : devices.filter(x => x.hub === selected.hub);
+
+  const duplicate = (
+    <div>
+      <nav className="navbar navbar-inverse navbar-fixed-top">
+          <div className="container">
+              <div className="navbar-header">
+                  <a className="navbar-brand" href="#">HA Bridge Configuration</a>
               </div>
+          </div>
+      </nav>
+      <div className="container">
+          <div className="panel panel-default bridgeServer">
+              <div className="panel-heading">
+                <h1 className="panel-title">Bridge settings</h1>
+              </div>
+              <div className="panel-body">
+                  <form className="form">
+                      <div className="col-xs-7 col-sm-7">
+                          <input id="bridge-base" className="form-control" type="text" placeholder="URL to bridge" />
+                      </div>
+                      <div className="btn-toolbar">
+                          <button type="submit" className="col-xs-2 col-sm-1 btn btn-primary btn">
+                            Load
+                          </button>
+                          <button type="submit" className="col-xs-2 col-sm-1 btn btn-primary">
+                            Go
+                          </button>
+                      </div>
+                  </form>
+              </div>
+          </div>
 
-			        <div className="panel panel-default">
-			            <div className="panel-heading">
-			            	<h2 className="panel-title">Current devices</h2>
-		            	</div>
-		            	<table className="table table-bordered table-striped table-hover">
-			                <thead>
-				                <tr>
-				                    <th>Name</th>
-				                    <th>Actions</th>
-				                </tr>
-			                </thead>
-			                <tbody>
-					            { ['_den', '_master_b_room', 'WifiPlugOne'].map((name, key) => {
-						            return (
-							                <tr key={key}>
-							                    <td>{name}</td>
-							                    <td>
-							                        <button className="btn btn-info" type="submit">ON</button>
-							                        <button className="btn btn-info" type="submit">OFF</button>
-							                        <button className="btn btn-danger" type="submit">Edit</button>
-							                        <button className="btn btn-danger" type="submit">Delete</button>
-							                    </td>
-							                </tr>
-						            	);
-						        	})
-					        	}
-				            </tbody>
-			            </table>
 
-			        </div>
-			    </div>
+          { !newHub &&
+          <div className="panel panel-default">
+              <div className="panel-heading">
+                <h2 className="panel-title">Hubs</h2>
+              </div>
+              <div className="panel-body">
+                <div className="form-group">
+                    <div className="col-xs-1"></div>
+                    <div className="col-xs-11">
+                      { hubs.map((name, key) => {
+                        return (
+                          <label className="radio" key={key}>
+                              <input value={name} type="radio" name="radio"
+                                checked={name===selected.hub}
+                                onChange={hubChange}
+                              />{name}
+                          </label>
+                        );
+                      })}
+                    </div>
+                    <div className="cols-xs-12 text-center">
+                      { !newHub &&
+                        <button className="btn" type="submit" onClick={handleAddHubClick}>Add Hub</button>
+                      }
+                    </div>
+                </div>
+              </div>
+          </div>
+        }{/* Hubs List */}
 
-			    <div>
-			        <div className="panel panel-default bridgeServer">
-			            <div className="panel-heading">
-			            	<h2 className="panel-title">New device</h2>
-		            	</div>
-			            <ul className="list-group">
-			                <li className="list-group-item">
-			                    <form className="form-horizontal">
-			                        <div className="form-group">
-			                            <div className="col-xs-8 col-sm-7">
-			                                <input type="text" className="form-control" id="device-name"placeholder="Device Name" />
-			                            </div>
-			                            <button type="submit" className="col-xs-3 col-sm-2 btn btn-primary">
-			                                Add Device
-			                            </button>
-			                        </div>
-			                        <div className="form-group">
-			                            <div className="col-xs-8 col-sm-7">
-			                                <input type="text" className="form-control" id="device-on-url" placeholder="URL to turn device on" />
-			                            </div>
-			                            <button className="col-xs-3 col-sm-2 btn btn-success" type="button">
-			                            	On Test
-			                            </button>
-			                        </div>
-			                        <div className="form-group">
-			                            <div className="col-xs-8 col-sm-7">
-			                                <input type="text" className="form-control" id="device-off-url" placeholder="URL to turn device off" />
-			                            </div>
-			                            <button className="col-xs-3 col-sm-2 btn btn-success" type="button">
-			                            	Off Test
-			                            </button>
-			                        </div>
-			                    </form>
-			                </li>
-			            </ul>
-			        </div>
-			    </div>
-			</div>
-		</div>
-	);
+          { newHub &&
+          <div className="panel panel-default">
+              <div className="panel-heading">
+                <h2 className="panel-title">New Hub</h2>
+              </div>
+              <div className="panel-body">
+                <form>
+                    <div className="form-group">
+                      <label htmlFor="hub-name">Name</label>
+                      <input type="text" className="form-control" id="hub-name" />
+                    </div>
+                    <div className="form-group">
+                      <label htmlFor="hub-pattern">URL Pattern</label>
+                      <input type="text" className="form-control" id="hub-pattern" />
+                    </div>
+                    <span className="col-xs-12">TODO: put preview here</span>
+                    <div className="form-group">
+                      <label htmlFor="exampleSelect1">Example select</label>
+                      <select className="form-control" id="exampleSelect1">
+                        <option>todo plugin type</option>
+                        <option>todo plugin type</option>
+                        <option>todo plugin type</option>
+                      </select>
+                    </div>
+                </form>
+                <div className="cols-xs-12 text-center">
+                  <button className="btn btn-danger cancel-hub" onClick={handleAddHubClick}>Cancel</button>
+                  <button className="btn btn-info submit-hub" type="submit" onClick={handleAddHubClick}>Add Hub</button>
+                </div>
+              </div>
+          </div>
+        }{/* new hub */}
 
-	return duplicate;
+          { currentDevices.length > 0 && !newHub &&
+          <div className="panel panel-default panel-success">
+              <div className="panel-heading">
+                <h2 className="panel-title">{selected.hub} devices</h2>
+              </div>
+              <table className="table table-bordered table-striped table-hover">
+                  <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Actions</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                  { currentDevices
+                    .map(x => x.name)
+                    .map((name, key) => {
+                      return (
+                        <tr key={key}>
+                            <td>{name}</td>
+                            <td className="text-center">
+                                <button className="btn btn-info" type="submit">ON</button>
+                                <button className="btn btn-info" type="submit">OFF</button>
+                                { selected.hub !== 'All' &&
+                                <button className="btn btn-danger" type="submit">Edit</button>
+                                }
+                                { selected.hub !== 'All' &&
+                                <button className="btn btn-danger" type="submit">Delete</button>
+                                }
+                            </td>
+                        </tr>
+                      );
+                    })
+                  }
+                </tbody>
+              </table>
+          </div>
+          }{/* currentDevices list */}
+
+          { selected.hub !== 'All' && !newHub &&
+          <div className="panel panel-default">
+              <div className="panel-heading">
+                <h2 className="panel-title">New {selected.hub} device</h2>
+              </div>
+              <ul className="list-group">
+                  <li className="list-group-item">
+                      <form className="form-horizontal">
+                          <div className="form-group">
+                              <div className="col-xs-8 col-sm-7">
+                                  <input type="text" className="form-control" id="device-name"placeholder="Device Name" />
+                              </div>
+                              <button type="submit" className="col-xs-3 col-sm-2 btn btn-primary">
+                                  Add Device
+                              </button>
+                          </div>
+                          <div className="form-group">
+                              <div className="col-xs-8 col-sm-7">
+                                  <input type="text" className="form-control" id="device-on-url" placeholder="URL to turn device on" />
+                              </div>
+                              <button className="col-xs-3 col-sm-2 btn btn-success" type="button">
+                                On Test
+                              </button>
+                          </div>
+                          <div className="form-group">
+                              <div className="col-xs-8 col-sm-7">
+                                  <input type="text" className="form-control" id="device-off-url" placeholder="URL to turn device off" />
+                              </div>
+                              <button className="col-xs-3 col-sm-2 btn btn-success" type="button">
+                                Off Test
+                              </button>
+                          </div>
+                      </form>
+                  </li>
+              </ul>
+          </div>
+          }{/* new device */}
+      </div>
+    </div>
+  );
+
+  return duplicate;
 }
 
 export default duplicate;
