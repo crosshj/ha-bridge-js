f7bc1df751d8039dd1f33ccdae56d6dbc9459245
crosshj <github@chimpjuice.com>
2017-02-23 23:14:27 -0500

    misc cleanup

--- client/components/navbar/homeIcon.js
+++ client/components/navbar/homeIcon.js
@@ -5,7 +5,6 @@ const HomeIcon = () => (
   <svg
     viewBox="0 0 30 30"
     width="15"
-    fill="#a6ffcf"
   >
     <g>
       <path d="M 15,0 L 0,14 V 30 H 11 V 18 H 19 V 30 H 30 V 14 Z"/>
--- client/components/navbar/index.js
+++ client/components/navbar/index.js
@@ -57,30 +57,3 @@ Navbar.propTypes = {
 };
 
 export default Navbar;
-
-
-/*
-<nav class="navbar navbar-inverse navbar-static-top" role="navigation">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-        </div>
-
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="navbar-collapse collapse" id="bs-example-navbar-collapse-1" aria-expanded="false" style="height: 1px;">
-        <ul class="nav navbar-nav">
-          <li><a href="index.php">Home</a></li>
-          <li><a href="about.php">About</a></li>
-          <li><a href="#portfolio">Portfolio</a></li>
-          <li><a href="#">Blog</a></li>
-          <li><a href="contact.php">Contact</a></li>
-        </ul>
-      </div>
-    </div>
-</nav>
-*/
